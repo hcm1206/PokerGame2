89f833c940307cddecf1d54581d92b6cf151fab9
hcm1206 <hcm1206@naver.com>
2023-07-31 23:08:14 +0900

    23-07-31 진행 내역 *기본적인 장고 요소 실습 중* - 장고 실습 테스트용 pybo 앱과 포커게임 개발용 pokergame 앱 생성 - pybo 앱과 pokergame 앱의 url 구성 - pybo 앱 테스트 모델 구성 - 장고 관리자 슈퍼유저 생성 - 모델을 이용한 pybo 앱 질문/답변 조회 기능 테스트 - 점프 투 장고 2장 01~05절 실습과 병행

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import pokergame.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pybo/', include('pybo.urls')),
+    path('pokergame/', pokergame.views.index),
 ]
